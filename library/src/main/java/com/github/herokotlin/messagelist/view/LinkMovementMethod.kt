d10195a5c7f010feb85ddc5b849cd9d6b7561231
musicode <musicode@qq.com>
2018-11-27 16:14:12 +0800

    update

---
+++ library/src/main/java/com/github/herokotlin/messagelist/view/LinkMovementMethod.kt
@@ -0,0 +1,90 @@
+package com.github.herokotlin.messagelist.view
+
+import android.text.Selection
+import android.text.Spannable
+import android.text.method.BaseMovementMethod
+import android.view.MotionEvent
+import android.widget.TextView
+
+object linkMovementMethod: BaseMovementMethod() {
+
+    private var linkSpan: LinkSpan? = null
+
+    private fun getLinkSpan(widget: TextView, text: Spannable, event: MotionEvent): LinkSpan? {
+
+        var x = event.x.toInt()
+        var y = event.y.toInt()
+
+        x -= widget.totalPaddingLeft
+        y -= widget.totalPaddingTop
+
+        x += widget.scrollX
+        y += widget.scrollY
+
+        val layout = widget.layout
+        val line = layout.getLineForVertical(y)
+        val off = layout.getOffsetForHorizontal(line, x.toFloat())
+
+        val link = text.getSpans(off, off, LinkSpan::class.java)
+        if (link.isNotEmpty()) {
+            return link[0]
+        }
+
+        return null
+
+    }
+
+    override fun onTouchEvent(widget: TextView, text: Spannable, event: MotionEvent?): Boolean {
+
+        val action = event?.actionMasked
+
+        return when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                val span = getLinkSpan(widget, text, event)
+                if (span != null) {
+                    span.isPressed = true
+                    Selection.setSelection(text, text.getSpanStart(span), text.getSpanEnd(span))
+                }
+                linkSpan = span
+                linkSpan != null
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val span = getLinkSpan(widget, text, event)
+                if (linkSpan != null && span != linkSpan) {
+                    linkSpan?.isPressed = false
+                    linkSpan = null
+                    Selection.removeSelection(text)
+                }
+                linkSpan != null
+            }
+
+            MotionEvent.ACTION_UP -> {
+                var result = false
+                if (linkSpan != null) {
+                    result = true
+                    linkSpan?.isPressed = false
+                    linkSpan?.onClick(widget)
+                    linkSpan = null
+                    Selection.removeSelection(text)
+                }
+                result
+            }
+
+            else -> {
+                if (linkSpan != null) {
+                    linkSpan?.isPressed = false
+                    linkSpan = null
+                    Selection.removeSelection(text)
+                }
+                false
+            }
+        }
+
+    }
+
+    override fun initialize(widget: TextView?, text: Spannable?) {
+        Selection.removeSelection(text)
+    }
+
+}
--- library/src/main/java/com/github/herokotlin/messagelist/view/LinkSpan.kt
+++ library/src/main/java/com/github/herokotlin/messagelist/view/LinkSpan.kt
@@ -1,11 +1,15 @@
 package com.github.herokotlin.messagelist.view
 
+import android.graphics.Color
 import android.text.TextPaint
 import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.View
 import com.github.herokotlin.messagelist.MessageListCallback
 
 class LinkSpan(val link: String, val callback: MessageListCallback): ClickableSpan() {
+
+    var isPressed = false
 
     override fun onClick(widget: View?) {
         callback.onLinkClick(link)
@@ -13,5 +17,12 @@ class LinkSpan(val link: String, val callback: MessageListCallback): ClickableSp
 
     override fun updateDrawState(ds: TextPaint?) {
         ds?.isUnderlineText = false
+        if (isPressed) {
+            ds?.bgColor = Color.RED
+        }
+        else {
+            ds?.bgColor = 0
+        }
+
     }
 }
